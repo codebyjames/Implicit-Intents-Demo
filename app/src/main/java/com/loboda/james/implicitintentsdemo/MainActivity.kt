f4bd5b3b3f78c135b8710468474a246bdc5a9f7b
James Loboda <papayev@Jamess-MacBook-Pro.local>
2022-02-24 18:30:35 -0500

    finish deprecated and current versions of activity result

--- app/src/main/java/com/loboda/james/implicitintentsdemo/MainActivity.kt
+++ app/src/main/java/com/loboda/james/implicitintentsdemo/MainActivity.kt
@@ -1,25 +1,74 @@
 package com.loboda.james.implicitintentsdemo
 
+/*
+ * Copyright (C) 2022 James Loboda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.loboda.james.implicitintentsdemo.databinding.ActivityMainBinding
 
+const val REQUEST_CODE_PHOTO = 0
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnTakePhoto.setOnClickListener {
-            Intent(Intent.ACTION_GET_CONTENT).also {
-                it.type = "image/*" // mime type any images image/jpg only looks for particular
-                startActivityFor
+        // new way of doing startActivityForResult
+        val startActivityNew = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent: Intent? = result.data
+                val data = intent?.data
+                data?.let {
+                    setImage(it)
+                }
+            } else {
+                Log.d("Stuff", "not successful")
             }
         }
 
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "image/*"
+        binding.btnTakePhoto.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                // mime type any images -- image/jpg only looks for particular
+                it.type = "image/*"
+//                startActivityForResult(it, REQUEST_CODE_PHOTO) // deprecated
+                startActivityNew.launch(it)
+            }
+        }
+    }
+
+    // Deprecated way
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_PHOTO) {
+            val uri = data?.data
+            uri?.let {
+                setImage(it)
+            }
+        }
+    }
+
+    private fun setImage(uri: Uri) {
+        binding.ivPhoto.setImageURI(uri)
     }
 }
